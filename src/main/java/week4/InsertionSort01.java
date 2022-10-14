d75add60f35875dd118ba404baa65b0a33d7c260
Hakjun <hiatus9014@gmail.com>
2022-10-14 10:07:25 +0900

    삽입 정렬

--- src/main/java/week4/InsertionSort01.java
+++ src/main/java/week4/InsertionSort01.java
@@ -5,8 +5,12 @@ import java.util.Arrays;
 public class InsertionSort01 {
     public int[] sort(int[] arr){
         int i = 1;
-
-        return new int[][];
+        if (arr[i] < arr[i - 1]) {
+            int temp = arr[i-1];
+            arr[i-1] = arr[i];
+            arr[i] = temp;
+        }
+        return arr;
     }
 
     public static void main(String[] args) {
