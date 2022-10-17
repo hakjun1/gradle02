94c7291d448f817d94b093ee800bae8f5bb332df
Hakjun <hiatus9014@gmail.com>
2022-10-17 09:36:47 +0900

    별찍기 n단

--- src/main/java/JavaExercise_9/RightTriangle.java
+++ src/main/java/JavaExercise_9/RightTriangle.java
@@ -3,13 +3,20 @@ package JavaExercise_9;
 import java.util.Scanner;
 
 public class RightTriangle {
+    private String letter = "*";
 
+    public RightTriangle() {
+
+    }
+    public RightTriangle(String letter) {
+        this.letter = letter;
+    }
 
     public void printStar(int input) {
 
         for (int i = 0; i < input; i++) {
             for (int j = 0; j <= i; j++) {
-                System.out.printf("%s","*");
+                System.out.print(this.letter);
             }
             System.out.println();
         }
