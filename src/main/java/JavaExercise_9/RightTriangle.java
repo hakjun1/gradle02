ee8a3bdfae26d30a7f6141e4dc835147d6d8a017
Hakjun <hiatus9014@gmail.com>
2022-10-17 09:21:25 +0900

    별찍기 n단

--- src/main/java/JavaExercise_9/RightTriangle.java
+++ src/main/java/JavaExercise_9/RightTriangle.java
@@ -1,14 +1,13 @@
 package JavaExercise_9;
 
+import java.util.Scanner;
+
 public class RightTriangle {
     public static void main(String[] args) {
-//
-//        System.out.println("*");
-//        System.out.println("**");
-//        System.out.println("***");
-//        System.out.println("****");
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
 
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < input; i++) {
             for (int j = 0; j <= i; j++) {
                 System.out.printf("%s","*");
             }
