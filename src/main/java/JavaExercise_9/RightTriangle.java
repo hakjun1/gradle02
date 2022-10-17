5acd35c7c8ee9ccb5fc916dd8166f63377b39b48
Hakjun <hiatus9014@gmail.com>
2022-10-17 09:07:50 +0900

    별찍기

---
+++ src/main/java/JavaExercise_9/RightTriangle.java
@@ -0,0 +1,13 @@
+package JavaExercise_9;
+
+public class RightTriangle {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 4; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
