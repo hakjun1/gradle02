a2bb4d985363f0f7ba94dae80b4d8ff01f2303ef
Hakjun <hiatus9014@gmail.com>
2022-10-17 11:49:03 +0900

    db연동

---
+++ src/main/java/com/dbexercise/UserDao.java
@@ -0,0 +1,35 @@
+package com.dbexercise;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Map;
+
+public class UserDao {
+    public void add() throws SQLException, ClassNotFoundException {
+        Map<String, String> env = System.getenv();
+        String dbHost = env.get("DB_HOST");
+        String dbUser = env.get("DB_USER");
+        String dbPassword =env.get("DB_PASSWORD");//
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection conn = DriverManager.getConnection(dbHost,dbUser,dbPassword);
+        PreparedStatement ps = conn.prepareStatement("INSERT INTO users(id, name,password) VALUES(?,?,?)");
+        ps.setString(1,"3");
+        ps.setString(2,"hakjun");
+        ps.setString(3,"1123");
+
+        ps.executeUpdate();//컨트롤 알트엔터
+        ps.close();
+        conn.close();
+    }
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        UserDao userDao = new UserDao();
+        userDao.add();
+
+    }
+}
+
