b9c03c3a16514a59c8f7c0553c3d2b497d47079e
Hakjun <hiatus9014@gmail.com>
2022-10-18 13:41:51 +0900

    abstract AwsUserDao

---
+++ src/main/java/com/dbexercise/domain/AwsUserDaoImpl.java
@@ -0,0 +1,18 @@
+package com.dbexercise.domain;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Map;
+
+public class AwsUserDaoImpl extends UserDaoAbstract{
+    @Override
+    public Connection makeConnection() throws SQLException {
+        Map<String, String> env = System.getenv();
+
+        Connection c = DriverManager.getConnection(env.get("DB_HOST"),
+                env.get("DB_USER"), env.get("DB_PASSWORD"));
+        return c;
+
+    }
+}
--- src/test/java/com/dbexercise/UserDaoTest.java
+++ src/test/java/com/dbexercise/UserDaoTest.java
@@ -1,5 +1,6 @@
 package com.dbexercise;
 
+import com.dbexercise.domain.AwsUserDaoImpl;
 import com.dbexercise.domain.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -12,11 +13,11 @@ class UserDaoTest {
 
     @Test
     void addAndSelect() throws SQLException, ClassNotFoundException {
-        UserDao userDao = new UserDao();
-        User user = new User("9","EternityHwan","1123");
-        userDao.add(user);
+        AwsUserDaoImpl userDao = new AwsUserDaoImpl();
+      // User user = new User("12","EternityHwan","1123");
+       userDao.add(new User("12","EternityHwan","1123"));
 
-        User selectedUser = userDao.findById("9");
+        User selectedUser = userDao.findById("12");
         Assertions.assertEquals("EternityHwan",selectedUser.getName());
 
     }
