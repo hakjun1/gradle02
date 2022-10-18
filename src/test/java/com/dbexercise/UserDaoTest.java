feafff9704e36d296f2a873f1df001c0ccb5e060
Hakjun <hiatus9014@gmail.com>
2022-10-18 12:28:21 +0900

    addAndSelect()

---
+++ src/test/java/com/dbexercise/UserDaoTest.java
@@ -0,0 +1,23 @@
+package com.dbexercise;
+
+import com.dbexercise.domain.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDaoTest {
+
+    @Test
+    void addAndSelect() throws SQLException, ClassNotFoundException {
+        UserDao userDao = new UserDao();
+        User user = new User("9","EternityHwan","1123");
+        userDao.add(user);
+
+        User selectedUser = userDao.findById("9");
+        Assertions.assertEquals("EternityHwan",selectedUser.getName());
+
+    }
+}
