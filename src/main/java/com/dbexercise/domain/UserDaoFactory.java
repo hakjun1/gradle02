d1e9f201ad7a52ec76500daf19e2c2320838e3bc
Hakjun <hiatus9014@gmail.com>
2022-10-19 14:21:31 +0900

    UsdrDao @Spring

--- src/main/java/com/dbexercise/domain/UserDaoFactory.java
+++ src/main/java/com/dbexercise/domain/UserDaoFactory.java
@@ -1,12 +1,18 @@
 package com.dbexercise.domain;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class UserDaoFactory {
     //조립을 해준다
+    @Bean
     public UserDaoAbstract awsUserDao() {//날개 5개 선풍기
         AwsConnectionMaker2 awsConnectionMaker2 = new AwsConnectionMaker2();
         UserDaoAbstract userDaoAbstract = new UserDaoAbstract(awsConnectionMaker2);
         return userDaoAbstract;
     }
+    @Bean
     public UserDaoAbstract localUserDao() {//날개 4개 선풍기
         UserDaoAbstract userDaoAbstract = new UserDaoAbstract((new LocalConnectionMaker2()));
         //위두줄을 간략화
--- src/test/java/com/dbexercise/UserDaoTest.java
+++ src/test/java/com/dbexercise/UserDaoTest.java
@@ -4,19 +4,35 @@ package com.dbexercise;
 import com.dbexercise.domain.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = UserDaoFactory.class)//이클래스를 특정해서
+
 
 class UserDaoTest {
+
+    @Autowired
+    ApplicationContext context;
 
     @Test
     void addAndSelect() throws SQLException, ClassNotFoundException {
      //   UserDaoAbstract userDao = new UserDaoAbstract(new LocalConnectionMaker2());
-      UserDaoAbstract userDao = new UserDaoFactory().awsUserDao();
+      UserDaoAbstract userDao = context.getBean("awsUserDao",UserDaoAbstract.class);
+      UserDaoAbstract userDao2 = context.getBean("awsUserDao",UserDaoAbstract.class);
+        //주소값 같음
+        System.out.println(userDao);
+        System.out.println(userDao2);
+
       //User.팩토리에서 사용
-       User user = new User("4","hakjun","1123");
+       User user = new User("5","hakjun","1123");
        userDao.add(user);
 
 //        User selectedUser = userDao.findById("8");
