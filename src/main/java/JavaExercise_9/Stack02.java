050716de2d23cf1462f777a8e69d13dc859e4fb3
Hakjun <hiatus9014@gmail.com>
2022-10-20 09:44:12 +0900

    stack 복습

---
+++ src/main/java/JavaExercise_9/Stack02.java
@@ -0,0 +1,30 @@
+package JavaExercise_9;
+
+public class Stack02 {
+    private Integer[] arr;
+    private int top = 0;
+
+    public Stack02() {
+        this.arr = new Integer[10000];
+    }
+
+    public Stack02(int size) {
+        this.arr = new Integer[size];
+    }
+
+    public Integer[] getArr() {
+        return arr;
+    }
+
+    public void push(int value) {
+        this.arr[top] = value;
+        this.top++;
+    }
+
+    public int pop() {
+        int value = this.arr[this.top-1];
+        this.top --;
+        return value;
+    }
+}
+
---
+++ src/test/java/JavaExercise_9/Stack02Test.java
@@ -0,0 +1,39 @@
+package JavaExercise_9;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Stack02Test {
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("before each");
+    }
+
+    @Test
+    @DisplayName("push 가 잘 되는지")
+    void push() {
+        Stack02 st = new Stack02();
+        st.push(10);
+        st.push(20);
+        Integer[] arr = st.getArr();
+        assertEquals(20, arr[1]);
+        assertEquals(10, arr[0]);
+    }
+
+    @Test
+    void pushAndPop() {
+
+        Stack02 st = new Stack02();
+        st.push(10);
+        st.push(20);
+        assertEquals(20, st.pop());
+        assertEquals(10, st.pop());
+
+    }
+
+
+}
