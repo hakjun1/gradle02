56a083ed908b9229550ed2736955abe381990fbb
Hakjun <hiatus9014@gmail.com>
2022-10-24 09:37:29 +0900

    k번째수

---
+++ src/main/java/JavaExercise_9/KthNumslice.java
@@ -0,0 +1,16 @@
+package JavaExercise_9;
+
+import java.util.Arrays;
+
+public class KthNumslice {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        for (int i = 0; i< commands.length;i++) {
+            int[] slicedArr = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            Arrays.sort(slicedArr);
+            answer[i] = slicedArr[commands[i][2]-1];
+        }
+        return answer;
+    }
+
+}
