9c05d9f415930632a5189718c822b47e9333b320
Hakjun <hiatus9014@gmail.com>
2022-10-27 09:57:01 +0900

    완주하지못한선수(중복고려안함)

---
+++ src/main/java/JavaExercise/ARacer.java
@@ -0,0 +1,32 @@
+package JavaExercise;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ARacer {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        Map<String, Integer> memo = new HashMap<>();
+        for (int i = 0; i < participant.length; i++) {
+            String key = participant[i];
+            memo.put(key, 1);//참여한선수 1을 준다
+        }
+        for (int i = 0; i < completion.length; i++) {
+            String key = completion[i];
+            memo.put(key, 0);//완주한선수 0을 준다
+        }
+        for (String key : memo.keySet()) {
+            if (memo.get(key) == 1) {//value가 1로 남아있는 key가 완주 못한선수
+                return key;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        ARacer aRacer = new ARacer();
+        String[] participant = {"leo", "kiki", "eden"};
+        String[] completion = {"eden", "kiki"};
+        System.out.println(aRacer.solution(participant,completion));
+    }
+}
