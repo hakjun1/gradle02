900f3c8b583772a180d37944247088b455601839
Hakjun <hiatus9014@gmail.com>
2022-10-31 10:19:34 +0900

    프로그래머스 모의고사(진행중)

--- src/main/java/JavaExercise/PrepareTest.java
+++ src/main/java/JavaExercise/PrepareTest.java
@@ -1,22 +1,25 @@
 package JavaExercise;
 
+import java.util.ArrayList;
+
 public class PrepareTest {
-    public int[] solution(int[] answer) {
+    public int[] solution(int[] answers) {
 
         /*
         1번 수포자가 찍는 방식: 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, ...
         2번 수포자가 찍는 방식: 2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5, ...
         3번 수포자가 찍는 방식: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ...
          */
-        //학생들 정답
+        //1.학생들 정답
         int[] student1 = {1,2,3,4,5}; //5
         int[] student2 = {2,1,2,3,2,4,2,5}; //8
         int[] student3 = {3,3,1,1,2,2,4,4,5,5}; //10
-        //카운트
+        //2.카운트
         int Student1AnsWerCnt = 0;
         int Student2AnsWerCnt = 0;
         int Student3AnsWerCnt = 0;
-        //나머지를 이용
+
+        //3.나머지를 이용
         for(int i=0; i<answers.length; i++){
             if (answers[i] == student1[i % 5]) {
                 Student1AnsWerCnt ++;
@@ -28,14 +31,20 @@ public class PrepareTest {
                 Student3AnsWerCnt ++;
             }
         }
+        //3.리스트를 이용해서 정렬
+        ArrayList<Integer> answerList = new ArrayList<>();
+        //...
 
-        return new int[0];
+        //4. answer배열에 담아서 리턴
+        int[] answer = new int[answerList.size()];
+        for(int i=0; i<answerList.size(); i++){
+            answer[i] = answerList.get(i);
+        }
+        return answer;
     }
 
     public static void main(String[] args) {
-        // 1번 answer --> 5
-        // 2번 answer --> 0
-    }
+         }
 
 
 
