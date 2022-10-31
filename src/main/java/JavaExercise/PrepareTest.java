0d41b34944976795934d7e07d2d1812f52e1d890
Hakjun <hiatus9014@gmail.com>
2022-10-31 10:47:47 +0900

    프로그래머스 모의고사

--- src/main/java/JavaExercise/PrepareTest.java
+++ src/main/java/JavaExercise/PrepareTest.java
@@ -1,6 +1,7 @@
 package JavaExercise;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class PrepareTest {
     public int[] solution(int[] answers) {
@@ -15,25 +16,30 @@ public class PrepareTest {
         int[] student2 = {2,1,2,3,2,4,2,5}; //8
         int[] student3 = {3,3,1,1,2,2,4,4,5,5}; //10
         //2.카운트
-        int Student1AnsWerCnt = 0;
-        int Student2AnsWerCnt = 0;
-        int Student3AnsWerCnt = 0;
-
+//        int Student1AnsWerCnt = 0;
+//        int Student2AnsWerCnt = 0;
+//        int Student3AnsWerCnt = 0;
+        int[] cnt = {0,0,0};
         //3.나머지를 이용
         for(int i=0; i<answers.length; i++){
             if (answers[i] == student1[i % 5]) {
-                Student1AnsWerCnt ++;
+                cnt[0]++;
             }
             if (answers[i] == student2[i % 8]) {
-                Student2AnsWerCnt ++;
+                cnt[1]++;
             }
             if (answers[i] == student3[i % 10]) {
-                Student3AnsWerCnt ++;
+                cnt[2]++;
             }
         }
-        //3.리스트를 이용해서 정렬
+        //3.리스트를 이용해서 정렬(Math.max)
+        int maxScore = Math.max(cnt[0],Math.max(cnt[1],cnt[2]));
         ArrayList<Integer> answerList = new ArrayList<>();
-        //...
+        for (int i = 0; i < cnt.length; i++) {
+            if (maxScore == cnt[i]) {
+                answerList.add(i+1);//0,1,2 이므로
+            }
+        }
 
         //4. answer배열에 담아서 리턴
         int[] answer = new int[answerList.size()];
@@ -44,7 +50,8 @@ public class PrepareTest {
     }
 
     public static void main(String[] args) {
-         }
+
+    }
 
 
 
