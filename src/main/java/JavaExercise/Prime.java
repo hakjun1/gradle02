15e00cb6a7cea0b81c2b0f2adc9641b32b70e840
Hakjun <hiatus9014@gmail.com>
2022-11-01 10:48:09 +0900

    템플릿적용 2단계 람다식적용

--- src/main/java/JavaExercise/Prime.java
+++ src/main/java/JavaExercise/Prime.java
@@ -3,11 +3,7 @@ package JavaExercise;
 public class Prime {
     //2부터 num까지 전부 나누고 나머지를 계산
     //template callback = 함수를 변수로 받고싶을때
-    boolean someOperation(int a, int b) {
-        return a < b;
-    }
-
-    boolean isPrime(StatementStrategy stmt){
+    boolean isPrime(int num,StatementStrategy stmt){
         for(int i=2; stmt.compare(i,num); i++){
             if (num % i == 0) {
                 return false;
@@ -16,17 +12,21 @@ public class Prime {
         return true;
     }
 
-
     public static void main(String[] args) {
         Prime prime = new Prime();
-        boolean r = prime.isPrime(17);
+
+        System.out.println(prime.isPrime(13,(a,b)->a<b));
+        System.out.println(prime.isPrime(17,(a,b)->a<b/2));
+        System.out.println(prime.isPrime(19,(a,b)->a*a<b));
+
+        boolean r = prime.isPrime(13, new StatementStrategy() {
+            @Override
+            public boolean compare(int a, int b) {
+                return a*a<=b;
+            }
+        });
         System.out.println(r);
 
-        System.out.println(prime.isPrime(13));
-        System.out.println(prime.isPrime(17));
-        System.out.println(prime.isPrime(19));
-
-        System.out.println(prime.isPrime(24));
     }
 }
 
