9a6ac4d12bcd8da95e8154a58cd70e1579e873de
Hakjun <hiatus9014@gmail.com>
2022-11-03 09:27:15 +0900

    에라토스테네스 100까지 진행중

---
+++ src/main/java/JavaExercise_9/MultipleOf.java
@@ -0,0 +1,13 @@
+package JavaExercise_9;
+
+public class MultipleOf {
+    public static void main(String[] args) {
+        //100까지
+        int cnt = 0;
+        boolean[] checks = new boolean[101];//배열인덱스 0 + 1
+        for (int i = 0; i < checks.length-1; i++) {
+
+        }
+    }
+
+}
