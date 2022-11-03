18fe9370c5a957664f2ee759c4893a8ff0c0c181
Hakjun <hiatus9014@gmail.com>
2022-11-03 09:50:06 +0900

    에라토스테네스 100까지 진행중

--- src/main/java/JavaExercise_9/MultipleOf.java
+++ src/main/java/JavaExercise_9/MultipleOf.java
@@ -1,13 +1,33 @@
 package JavaExercise_9;
 
+import java.util.Arrays;
+
 public class MultipleOf {
     public static void main(String[] args) {
         //100까지
-        int cnt = 0;
-        boolean[] checks = new boolean[101];//배열인덱스 0 + 1
-        for (int i = 0; i < checks.length-1; i++) {
-
+        int cnt = 100;
+        int[] nums = new int[cnt-1];//N-1개 만큼 2~50 49개
+        boolean[] checks = new boolean[nums.length];
+        Arrays.fill(checks,true);
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = i + 2;
         }
+
+
+        //2의배수 , 4 , 6 , 8     //
+        for (int i = 2; i < cnt; i+=2) {
+            checks[i] = false;
+        }
+        for (int i = 4; i < cnt; i+=3) {
+            checks[i] = false;
+        }
+        for (int i = 6; i < cnt; i+=4) {
+            checks[i] = false;
+        }
+        for (int i = 8; i < cnt; i+=5) {
+            checks[i] = false;
+        }
+
     }
 
 }
