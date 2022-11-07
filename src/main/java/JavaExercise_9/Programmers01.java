184de9d511a5ba745632277964fff099d76363b0
Hakjun <hiatus9014@gmail.com>
2022-11-07 09:30:40 +0900

    프로그래머스 하샤드수 진행

--- src/main/java/JavaExercise_9/Programmers01.java
+++ src/main/java/JavaExercise_9/Programmers01.java
@@ -8,13 +8,14 @@ class Solution1 {
         int sum = 0;
         int num = x;
         boolean answer = true;
-        while ((x != 0)) {
-            sum += x%10;
-            x/=10;
+        while (x > 0) {
+            sum += x%10; //나머지
+            x/=10; //몫 저장
         }
-        if (num % sum != 0)
+        if (num % sum > 0)
             answer = false;
 
+        System.out.println(answer);
         return answer;
     }
 
@@ -24,7 +25,7 @@ public class Programmers01 {
     public static void main(String[] args) {
         Solution1 solution1 = new Solution1();
 
-        int input = 18;
+        int input = 13;
         solution1.solution(input);
 
     }
