c6b76e557989bbae46f49e5da3e441f988db33b6
Hakjun <hiatus9014@gmail.com>
2022-11-22 09:21:44 +0900

    재귀 2 : 탈출조건넣기

--- src/main/java/week4/java/Print1To100.java
+++ src/main/java/week4/java/Print1To100.java
@@ -2,8 +2,9 @@ package week4.java;
 
 public class Print1To100 {
     public static void print1to(int n) {
+        if (n>100) return;
         System.out.println(n);
-        print1to(n);
+        print1to(n+1);
     }
 
     public static void main(String[] args) {
