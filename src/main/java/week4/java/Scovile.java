ef36a7eb647da24e68efabe40db6d9b0df8116b7
Hakjun <hiatus9014@gmail.com>
2022-12-07 10:18:03 +0900

    프로그래머스 더 맵게(런타임에러) 우선순위큐 사용

---
+++ src/main/java/week4/java/Scovile.java
@@ -0,0 +1,24 @@
+package week4.java;
+
+import java.util.PriorityQueue;
+
+public class Scovile {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->a-b);
+        for (int i = 0; i < scoville.length; i++) {
+            pq.add(scoville[i]);//우선순위 큐에 넣는다
+        }
+        while (pq.size() >= 2 && pq.peek() < K) {
+            int mix= pq.poll()+(pq.poll()*2);
+            //맨앞에있는게 K보다 작을때 섞는다
+            pq.add(mix);
+            answer++;
+        }
+        return answer;
+
+    }
+
+}
+//문제 1 런타임에러
